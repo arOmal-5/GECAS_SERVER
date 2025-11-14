e7d7432927a3902f1ef69e14611b2dd745d4ed66
apple <apple@MacBook-Air.local>
2025-11-14 08:14:02 +0530

    portadded

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
+import express from "express";
 import dotenv from "dotenv";
 import { reminderJob } from "./jobs/calendarCheck.js";
 
-
 dotenv.config();
 
-console.log("🕒 Starting reminder cron job...");
+const app = express();
+
+// Start cron
 reminderJob.start();
+console.log("Cron job started");
+
+// Start server
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on port", process.env.PORT || 5000);
+});
